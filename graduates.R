6fbfdfce2ba6ab92d9b5874bffed7e400ba15442
Eleanor Murray <emurray@mail.harvard.edu>
2019-02-17 14:39:25 -0500

    Formatted year into a numeric variable, by changing to character, stripping leading "X", and changing to a number.
    Altered plot to only show 3 x-axis labels for better readability.
    
    GGPLOT still won't let me plot this as a line though. Ideas, anyone?

--- graduates.R
+++ graduates.R
@@ -2,12 +2,12 @@
 
 ##2/16/19
 library(tidyverse)
-library(openxlsx)
+library(here)
 
 #url <- "https://ncses.nsf.gov/pubs/nsf19301/assets/data/tables/sed17-sr-tab013.xlsx"
 
 file_name<-"sed17-sr-tab013.csv"
-finefield<-read.csv(here("dataviz_challenge", file_name))
+finefield<-read.csv(here(file_name))
 names(finefield)
 
 epi <- subset(finefield, subset = Field %in% "Epidemiology")
@@ -20,4 +20,22 @@ g+geom_point(color="gray70") +
        title ="Epidemiology Doctoral Degrees 2008-2017")+
   theme_bw()
 
-ggsave(here("dataviz_challenge", "epi.png"))
+ggsave(here("epi.png"))
+
+#Suggested improvements
+#1. change year to a numeric or date variable
+class(epi_long$year)
+epi_long$year_c <- as.character(epi_long$year)
+epi_long$year_c2 <- substr(epi_long$year_c, 2, nchar(epi_long$year_c))
+epi_long$year_num <- as.numeric(epi_long$year_c2)
+
+library(ggrepel)
+
+g <- ggplot(data=epi_long, mapping=aes(x=year_num, y=graduates))
+g+geom_point()+
+  scale_x_continuous(breaks=c(2008, 2012, 2016), labels = c("2008", "2012", "2016"))+
+  labs(x="Year", y = "# graduates", title = "Epidemiology Doctoral Degrees 2008-2017", 
+       caption="Data from nsf.gov")+
+  theme_bw()
+
+ggsave(here("epi2.png"))
